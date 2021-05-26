ad4d87ce7c2b7b246ac96d962792d2123220a2b0
Teo <teodelrey20@gmail.com>
2021-05-26 17:38:37 +0200

    Recorre las 7 paginas de Licitacion Publica Tecnologica

--- Google.py
+++ Google.py
@@ -17,7 +17,7 @@ options =  webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 
-driver_path = 'D:\\Forks\\GoogleScrap\\chromedriver_win32\\chromedriver.exe'
+driver_path = 'C:\\Users\\teode\\GoogleScrap\\chromedriver_win32\\chromedriver.exe'
 
 searchterm = input("¿Que quieres buscar?: ")
 searchterm = searchterm.replace(" ", "+")
@@ -31,10 +31,9 @@ soup = BeautifulSoup(r.text, "html.parser")
 productslist = []
 descripciones = []
 '''i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]'''
-i = [1,50]
+i = [1,2,3,4,5,7,8,9,10]
 print(i)
 results = soup.find_all("div", {"class": "s-item__info clearfix"})
-j = [1,2,3,4,5,6]
 
 try:
     WebDriverWait(driver, 5) \
@@ -48,7 +47,11 @@ except:
 #Analiza la primera pagina
 estado = 2
 
-WebDriverWait(driver, 1) \
-    .until(EC.element_to_be_clickable((By.CLASS_NAME,
-                                'yuRUbf'))) \
-    .click()
+for j in i:
+    WebDriverWait(driver, 5) \
+        .until(EC.element_to_be_clickable((By.XPATH,
+                                    '//*[@id="rso"]/div['+str(j)+']/div/div/div[1]'))) \
+        .click()
+    driver.back()
+    
+
