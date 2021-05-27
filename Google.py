1ce0fae60c034faef1656aec2d1aeb2167dc2c43
Teo <teodelrey20@gmail.com>
2021-05-27 11:19:19 +0200

    Login + Cookies

--- Google.py
+++ Google.py
@@ -17,9 +17,9 @@ options =  webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 
-driver_path = 'D:\\Forks\\GoogleScrap\\chromedriver_win32\\chromedriver.exe'
+driver_path = 'C:\\Users\\Teo\\GoogleScrap\\chromedriver_win32\\chromedriver.exe'
 
-url = "https://motionarray.com/davinci-resolve-templates/photo-gallery-family-memories-737938/"
+url = "https://motionarray.com/account/login"
 
 driver = webdriver.Chrome(driver_path, chrome_options=options)
 
@@ -28,12 +28,27 @@ r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 '''results = soup.find_all("div", {"class": "s-item__info clearfix"})'''
 
-WebDriverWait(driver, 5) \
-.until(EC.element_to_be_clickable((By.XPATH,
-                                     '//*[@id="download-btn-container"]/a'))) \
-.click()
-
+#Aceptamos las cookies
 WebDriverWait(driver, 5) \
 .until(EC.element_to_be_clickable((By.XPATH,
                                      '//*[@id="adroll_reject"]'))) \
 .click()
+
+#Nos logueamos y entramos en la web
+correo = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/form/ul/li[1]/input")
+correo.send_keys("nitro85@gmail.com")
+
+contraseña = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/form/ul/li[2]/input")
+contraseña.send_keys("XXLxxl123!")
+
+WebDriverWait(driver, 5) \
+.until(EC.element_to_be_clickable((By.XPATH,
+                                     '//*[@id="site-app"]/div[2]/div[2]/div[2]/form/ul/li[4]/button'))) \
+.click()
+
+#Hacemos click y seleccionamos Davinci templates
+
+
+
+
+
