805faaf94ccfec66be2da01624a9a3a2f44b29b2
Teo <teodelrey20@gmail.com>
2021-05-27 13:15:00 +0200

    Funcionando

--- Google.py
+++ Google.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -5,14 +7,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
 import pandas as pd
 import datetime
-espacio = '   '
 
-titles=[]
-prices=[]
-link=[]
+
 
 # Opciones de navegación
 options =  webdriver.ChromeOptions()
@@ -29,6 +27,7 @@ driver.get( url )
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 '''results = soup.find_all("div", {"class": "s-item__info clearfix"})'''
+i = range(1,6)
 
 #Aceptamos las cookies
 WebDriverWait(driver, 5) \
@@ -49,12 +48,12 @@ WebDriverWait(driver, 5) \
 .click()
 
 #Aceptamos cookies y quitamos publicidad
-WebDriverWait(driver, 5) \
+WebDriverWait(driver, 1) \
     .until(EC.element_to_be_clickable((By.XPATH,
                                        '//*[@id="cookiescript_buttons"]'))) \
     .click()
 
-WebDriverWait(driver, 5) \
+WebDriverWait(driver, 1) \
     .until(EC.element_to_be_clickable((By.XPATH,
                                        '//*[@id="browse"]/div[5]/div/button'))) \
     .click()
@@ -70,3 +69,76 @@ WebDriverWait(driver, 5) \
                                        '/html/body/div[1]/div[2]/div[2]/div[1]/section/div/div[1]/div[2]/div/ul[8]/li/ul/li[1]/div/label/span[2]'))) \
     .click()
 
+#Bucle
+for j in i:
+    WebDriverWait(driver, 500) \
+        .until(EC.element_to_be_clickable((By.XPATH,
+                                           '//*[@id="right-column"]/div[1]/div['+str(j)+']/div[2]/h2'))) \
+        .click()
+    WebDriverWait(driver, 500) \
+        .until(EC.element_to_be_clickable((By.XPATH,
+                                           '//*[@id="download-btn-container"]/a'))) \
+        .click()
+    time.sleep(3)
+    driver.back()
+
+#Segunda vez
+
+driver.get( url )
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+'''results = soup.find_all("div", {"class": "s-item__info clearfix"})'''
+i = range(7,14)
+
+#Aceptamos las cookies
+WebDriverWait(driver, 5) \
+.until(EC.element_to_be_clickable((By.XPATH,
+                                     '//*[@id="adroll_reject"]'))) \
+.click()
+
+#Nos logueamos y entramos en la web
+correo = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/form/ul/li[1]/input")
+correo.send_keys("nitro85@gmail.com")
+
+contraseña = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[2]/form/ul/li[2]/input")
+contraseña.send_keys("XXLxxl123!")
+
+WebDriverWait(driver, 5) \
+.until(EC.element_to_be_clickable((By.XPATH,
+                                     '//*[@id="site-app"]/div[2]/div[2]/div[2]/form/ul/li[4]/button'))) \
+.click()
+
+#Aceptamos cookies y quitamos publicidad
+WebDriverWait(driver, 1) \
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                       '//*[@id="cookiescript_buttons"]'))) \
+    .click()
+
+WebDriverWait(driver, 1) \
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                       '//*[@id="browse"]/div[5]/div/button'))) \
+    .click()
+
+#Hacemos click y seleccionamos Davinci templates
+WebDriverWait(driver, 5) \
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                       '/html/body/div[1]/div[2]/div[2]/div[1]/section/div/div[1]/div[2]/div/ul[8]/li/div[1]/div/label'))) \
+    .click()
+
+WebDriverWait(driver, 5) \
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                       '/html/body/div[1]/div[2]/div[2]/div[1]/section/div/div[1]/div[2]/div/ul[8]/li/ul/li[1]/div/label/span[2]'))) \
+    .click()
+
+#Bucle
+for j in i:
+    WebDriverWait(driver, 500) \
+        .until(EC.element_to_be_clickable((By.XPATH,
+                                           '//*[@id="right-column"]/div[1]/div['+str(j)+']/div[2]/h2'))) \
+        .click()
+    WebDriverWait(driver, 500) \
+        .until(EC.element_to_be_clickable((By.XPATH,
+                                           '//*[@id="download-btn-container"]/a'))) \
+        .click()
+    time.sleep(3)
+    driver.back()
